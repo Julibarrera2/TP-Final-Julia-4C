7bc95ab07e9a1a23ba419206b24aa3a22bc695b6
Julibarrera2 <164010054+Julibarrera2@users.noreply.github.com>
2024-06-18 08:50:09 -0300

    Initial commit

---
+++ Assets/scripts/PasarObjects.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasarObjects : MonoBehaviour
+{
+    public GameObject[] alimentos;
+    public int currentIndex = -1;
+
+    void Start()
+    {
+        DeactivateAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            currentIndex++;
+            if (currentIndex < alimentos.Length)
+            {
+                ActivateByIndex(currentIndex);
+            }
+            if (currentIndex > alimentos.Length - 1)
+            {
+                currentIndex = -1;
+                DeactivateAll();
+            }
+        }
+    }
+    void DeactivateAll()
+    {
+        for (int i = 0; i < alimentos.Length; i++)
+        {
+            alimentos[i].SetActive(false);
+        }
+    }
+
+    void ActivateByIndex(int index)
+    {
+        alimentos[index].SetActive(true);
+    }
+
+}
