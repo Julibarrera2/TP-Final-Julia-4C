4479723637f7399e8d1065b6aeeef7d23ed92eaa
Julibarrera2 <164010054+Julibarrera2@users.noreply.github.com>
2024-06-18 08:59:03 -0300

    Ejercicio resuelto

--- Assets/scripts/PasarObjects.cs
+++ Assets/scripts/PasarObjects.cs
@@ -5,31 +5,48 @@ using UnityEngine;
 public class PasarObjects : MonoBehaviour
 {
     public GameObject[] alimentos;
-    public int currentIndex = -1;
-
+    public int currentsIndex = 10;
     void Start()
     {
         DeactivateAll();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-
-            currentIndex++;
-            if (currentIndex < alimentos.Length)
-            {
-                ActivateByIndex(currentIndex);
-            }
-            if (currentIndex > alimentos.Length - 1)
-            {
-                currentIndex = -1;
-                DeactivateAll();
-            }
+            ActivatePrevious();
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ActivateNext();
+        }
+
     }
+
+    public void ActivateNext()
+    {
+        currentsIndex++;
+        if (currentsIndex >= alimentos.Length)
+        {
+            currentsIndex = 0;
+        }
+       
+        DeactivateAll();
+        alimentos[currentsIndex].SetActive(true);
+    }
+
+    public void ActivatePrevious()
+    {
+        currentsIndex--;
+        if (currentsIndex < 0)
+        {
+            currentsIndex = alimentos.Length - 1;
+        }
+        DeactivateAll();
+        alimentos[currentsIndex].SetActive(true);
+    }
+
+
     void DeactivateAll()
     {
         for (int i = 0; i < alimentos.Length; i++)
@@ -37,10 +54,4 @@ public class PasarObjects : MonoBehaviour
             alimentos[i].SetActive(false);
         }
     }
-
-    void ActivateByIndex(int index)
-    {
-        alimentos[index].SetActive(true);
-    }
-
 }
